aa91567d13b65ccbbdb2cba6a5a9479a4de6ef41
Diogo Teixeira <diogo.teixeira@smark.io>
2020-03-24 18:14:07 +0000

    Add format and message arguments, remove filename argument.

--- app.js
+++ app.js
@@ -3,51 +3,31 @@
 // server
 const express = require('express');
 const app = express();
-const path = require('path');
-
-const tmpDir = __dirname + '/tmp/';
-const publicDir = __dirname + '/public/';
 
 // canvas generator
 const CountdownGenerator = require('./countdown-generator');
 
-app.use(express.static(publicDir));
-app.use(express.static(tmpDir));
-
-// root
-app.get('/', function (req, res) {
-    res.sendFile(publicDir + 'index.html');
-});
-
 // generate and download the gif
 app.get('/generate', function (req, res) {
-    let {time, width, height, color, bg, name, frames} = req.query;
-
-    if(!time){
+    let { time, width, height, color, bg, frames, format, message } = req.query;
+    if (!time) {
         throw Error('Time parameter is required.');
     }
-
-    CountdownGenerator.init(time, width, height, color, bg, name, frames, () => {
-        let filePath = tmpDir + name + '.gif';
-        res.download(filePath);
-    });
+    res.set({ 'Content-type': 'application/octet-stream', 'Content-disposition': 'attachment; filename=countdown.gif' });
+    CountdownGenerator.init(time, width, height, color, bg, frames, format, message, res);
 });
 
 // serve the gif to a browser
 app.get('/serve', function (req, res) {
-    let {time, width, height, color, bg, name, frames} = req.query;
-
-    if(!time){
+    let { time, width, height, color, bg, frames, format, message } = req.query;
+    if (!time) {
         throw Error('Time parameter is required.');
     }
-
-    CountdownGenerator.init(time, width, height, color, bg, name, frames, () => {
-        let filePath = tmpDir + name + '.gif';
-        res.sendFile(filePath);
-    });
+    res.type('gif');
+    CountdownGenerator.init(time, width, height, color, bg, frames, format, message, res);
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
--- countdown-generator/index.js
+++ countdown-generator/index.js
@@ -1,112 +1,105 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
 const GIFEncoder = require('gifencoder');
 const Canvas = require('canvas');
 const moment = require('moment');
 
 module.exports = {
-    /**
-     * Initialise the GIF generation
-     * @param {string} time
-     * @param {number} width
-     * @param {number} height
-     * @param {string} color
-     * @param {string} bg
-     * @param {string} name
-     * @param {number} frames
-     * @param {requestCallback} cb - The callback that is run once complete.
-     */
-    init: function(time, width=200, height=200, color='000000', bg='ffffff', name='default', frames=30, cb){
-        // Set some sensible upper / lower bounds
+    init: function (time, width = 200, height = 200, color = '000000', bg = 'ffffff', frames = 30, format = '%H:%M:%S', message = 'Expired!', response) {
         this.width = this.clamp(width, 150, 500);
         this.height = this.clamp(height, 150, 500);
         this.frames = this.clamp(frames, 1, 90);
-        
+        this.format = this.clampString(format, 80);
+        this.message = this.clampString(message, 100);
+        this.response = response;
         this.bg = '#' + bg;
         this.textColor = '#' + color;
-        this.name = name;
-        
-        // loop optimisations
+
         this.halfWidth = Number(this.width / 2);
         this.halfHeight = Number(this.height / 2);
-        
         this.encoder = new GIFEncoder(this.width, this.height);
         this.canvas = new Canvas(this.width, this.height);
         this.ctx = this.canvas.getContext('2d');
-        
-        // calculate the time difference (if any)
-        let timeResult = this.time(time);
-        
-        // start the gif encoder
-        this.encode(timeResult, cb);
+
+        this.encode(this.time(time));
     },
-    /**
-     * Limit a value between a min / max
-     * @link http://stackoverflow.com/questions/11409895/whats-the-most-elegant-way-to-cap-a-number-to-a-segment
-     * @param number - input number
-     * @param min - minimum value number can have
-     * @param max - maximum value number can have
-     * @returns {number}
-     */
-    clamp: function(number, min, max){
+    clamp: function (number, min, max) {
         return Math.max(min, Math.min(number, max));
     },
-    /**
-     * Calculate the diffeence between timeString and current time
-     * @param {string} timeString
-     * @returns {string|Object} - return either the date passed string, or a valid moment duration object
-     */
+    clampString: function (string, max) {
+        return string.substring(0, Math.min(max, string.length));
+    },
     time: function (timeString) {
-        // grab the current and target time
         let target = moment(timeString);
         let current = moment();
-        
-        // difference between the 2 (in ms)
-        let difference = target.diff(current);
-        
-        // either the date has passed, or we have a difference
-        if(difference <= 0){
-            return 'A reserva expirou!';
-        } else {
-            // duration of the difference
-            return moment.duration(difference);
-        }
+        return moment.duration(target.diff(current));
     },
-    /**
-     * Encode the GIF with the information provided by the time function
-     * @param {string|Object} timeResult - either the date passed string, or a valid moment duration object
-     * @param {requestCallback} cb - the callback to be run once complete
-     */
-    encode: function(timeResult, cb){
+    addFrame(text) {
         let enc = this.encoder;
         let ctx = this.ctx;
-        let tmpDir = process.cwd() + '/tmp/';
 
-        // create the tmp directory if it doesn't exist
-        if (!fs.existsSync(tmpDir)){
-            fs.mkdirSync(tmpDir);
+        // calculate optimized font size
+        let minMargin = 20, minFontSize = 10, font = 'Arial', fontSize = Math.floor(this.width / (text.length * 0.60));
+        fontSize = Math.max(minFontSize, fontSize);
+        while (fontSize > minFontSize) {
+            ctx.font = fontSize + 'px ' + font;
+            let textWidth = ctx.measureText(text).width
+            if (textWidth + minMargin <= this.width) {
+                break;
+            }
+            fontSize = fontSize - 1;
         }
-        
-        let filePath = tmpDir + this.name + '.gif';
-        
-        // pipe the image to the filesystem to be written
-        let imageStream = enc
-                .createReadStream()
-                    .pipe(fs.createWriteStream(filePath));
-        // once finised, generate or serve
-        imageStream.on('finish', () => {
-            // only execute callback if it is a function
-            typeof cb === 'function' && cb();
-        });
-        
-        // estimate the font size based on the provided width
-        let fontSize = Math.floor(this.width / 8) + 'px';
-        let fontFamily = 'Arial'; // monospace works slightly better
-        
-        // set the font style
-        ctx.font = [fontSize, fontFamily].join(' ');
+        ctx.font = fontSize + 'px ' + font;
+
+        // paint the frame
+        ctx.fillStyle = this.bg;
+        ctx.fillRect(0, 0, this.width, this.height);
+        ctx.fillStyle = this.textColor;
+        ctx.fillText(text, this.halfWidth, this.halfHeight);
+        enc.addFrame(ctx);
+    },
+    encodeDuration(duration) {
+        if (!moment.isDuration(duration) || duration.asMilliseconds() < 0) {
+            return this.message;
+        }
+
+        // get time parts
+        let days = Math.floor(duration.asDays());
+        let hours = Math.floor(duration.asHours() - (days * 24));
+        let minutes = Math.floor(duration.asMinutes()) - (days * 24 * 60) - (hours * 60);
+        let seconds = Math.floor(duration.asSeconds()) - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
+        let hoursPadded = (hours.toString().length == 1) ? '0' + hours : hours;
+        let minutesPadded = (minutes.toString().length == 1) ? '0' + minutes : minutes;
+        let secondsPadded = (seconds.toString().length == 1) ? '0' + seconds : seconds;
+        let timeParts = [
+            [/%d/g, days],
+            [/%h/g, hours],
+            [/%m/g, minutes],
+            [/%s/g, seconds],
+            [/%H/g, hoursPadded],
+            [/%M/g, minutesPadded],
+            [/%S/g, secondsPadded]
+        ];
+
+        // replace time parts
+        let result = this.format;
+        for (let i = 0, len = timeParts.length; i < len; i++) {
+            result = result.replace(timeParts[i][0], timeParts[i][1]);
+        }
+        // clear out any 0 groups and limiters
+        result = result.replace(/\[0+:[^\]]*\]/g, '');
+        result = result.replace(/\[\d+:([^\]]*)\]/g, '$1');
+
+        return result;
+    },
+    encode: function (duration) {
+        let enc = this.encoder;
+        let ctx = this.ctx;
+
+        // stream response
+        enc.createReadStream().pipe(this.response);
+
+        // set text alignment
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
 
@@ -116,53 +109,19 @@ module.exports = {
         enc.setDelay(1000);
         enc.setQuality(10);
 
-        // if we have a moment duration object
-        if(typeof timeResult === 'object'){
-            for(let i = 0; i < this.frames; i++){
-                // extract the information we need from the duration
-                let days = Math.floor(timeResult.asDays());
-                let hours = Math.floor(timeResult.asHours() - (days * 24));
-                let minutes = Math.floor(timeResult.asMinutes()) - (days * 24 * 60) - (hours * 60);
-                let seconds = Math.floor(timeResult.asSeconds()) - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
-                
-                // make sure we have at least 2 characters in the string
-                days = (days.toString().length == 1) ? '0' + days : days;
-                hours = (hours.toString().length == 1) ? '0' + hours : hours;
-                minutes = (minutes.toString().length == 1) ? '0' + minutes : minutes;
-                seconds = (seconds.toString().length == 1) ? '0' + seconds : seconds;
-                
-                // build the date string
-                //let string = [days, 'd ', hours, 'h ', minutes, 'm ', seconds, 's'].join('');
-                let string = [hours, 'h ', minutes, 'm ', seconds, 's'].join('');
-                
-                // paint BG
-                ctx.fillStyle = this.bg;
-                ctx.fillRect(0, 0, this.width, this.height);
-                
-                // paint text
-                ctx.fillStyle = this.textColor;
-                ctx.fillText(string, this.halfWidth, this.halfHeight);
-                
-                // add finalised frame to the gif
-                enc.addFrame(ctx);
-                
-                // remove a second for the next loop
-                timeResult.subtract(1, 'seconds');
-            }
-        } else {
-            // Date has passed so only using a string
-            
-            // BG
-            ctx.fillStyle = this.bg;
-            ctx.fillRect(0, 0, this.width, this.height);
-            
-            // Text
-            ctx.fillStyle = this.textColor;
-            ctx.fillText(timeResult, this.halfWidth, this.halfHeight);
-            enc.addFrame(ctx);
+        // optimize expired countdowns with a single frame
+        if (duration.asMilliseconds() < 0) {
+            this.addFrame(this.message);
+            enc.finish();
+            return;
         }
-        
-        // finish the gif
+
+        // generate a frame per second, up to the total frame number
+        for (let i = 0; i < this.frames; i++) {
+            this.addFrame(this.encodeDuration(duration));
+            duration.subtract(1, 'seconds');
+        }
+
         enc.finish();
     }
 };
